3ff500d131e2eee954c05dc31a21d9f2c369304b
alec <alec@prismlabs.tech>
2024-10-03 10:43:20 -0700

    Initial Commit

---
+++ SeekrTests/SeekrTests.swift
@@ -0,0 +1,17 @@
+//
+//  SeekrTests.swift
+//  SeekrTests
+//
+//  Created by Alec Raymond on 10/3/24.
+//
+
+import Testing
+@testable import Seekr
+
+struct SeekrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
