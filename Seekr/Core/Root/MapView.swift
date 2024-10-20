2a63e351586a48772a53a742c45b0cc66b6ddd31
Taya Ambrose <tayaambrose@Tayas-MacBook-Pro.local>
2024-10-20 12:30:17 -0700

    readme test

---
+++ Seekr/Core/Root/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Seekr
+//
+//  Created by Taya Ambrose on 10/19/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
