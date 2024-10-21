eff43d670048442ca9e3e3e55b2f388bbf651941
alec <alec@prismlabs.tech>
2024-10-21 09:56:30 -0700

    Basic Map Functionality

--- Seekr/ContentView.swift
+++ Seekr/ContentView.swift
@@ -1,38 +1,16 @@
-//
-//  ContentView.swift
-//  Seekr
-//
-//  Created by Alec Raymond on 10/3/24.
-//
-
 import SwiftUI
-import CoreData
-import MapKit
-
-extension CLLocationCoordinate2D {
-    static let ucsc = CLLocationCoordinate2D(latitude: 36.9905, longitude: -122.0584)
-}
 
 struct ContentView: View {
     var body: some View {
-        Map {
-            Annotation("UCSC", coordinate: .ucsc) {
-                ZStack{
-                    RoundedRectangle(cornerRadius: 5)
-                        .fill(.background)
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(.secondary, lineWidth: 5)
-                    Image(systemName: "car")
-                        .padding(5)
-                }
-            }
-            .annotationTitles(.hidden)
-        }
+        ViewControllerWrapper()
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ViewControllerWrapper: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> ViewController {
+        return ViewController()
     }
+
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {}
 }
---
+++ Seekr/ViewController.swift
@@ -0,0 +1,200 @@
+//
+//  ViewController.swift
+//  Seekr
+//
+//  Created by Alec Raymond on 10/19/24.
+//x 
+import UIKit
+import CoreData
+import MapKit
+import SwiftUI
+import CoreLocation
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate, UISearchBarDelegate, MKLocalSearchCompleterDelegate, UITableViewDataSource, UITableViewDelegate {
+    
+    var searchCompleter = MKLocalSearchCompleter()
+    var searchResults = [MKLocalSearchCompletion]()
+    var annotationList = [MKPointAnnotation]()
+    var tableView = UITableView()
+    
+    let geocoder = CLGeocoder()
+    
+    let locationManager = CLLocationManager()
+    lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        map.showsUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    
+    lazy var searchTextField: UISearchBar = {
+        let searchTextField = UISearchBar()
+        searchTextField.layer.cornerRadius = 15
+        searchTextField.clipsToBounds = true
+        searchTextField.backgroundColor = UIColor.lightGray
+        searchTextField.placeholder = "Search"
+        searchTextField.delegate = self
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        return searchTextField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        searchCompleter.delegate = self
+        
+        tableView.delegate = self // Set the delegate
+        tableView.dataSource = self
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.addSubview(mapView)
+        view.addSubview(searchTextField)
+        view.addSubview(tableView)
+        
+        searchTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width/1.2).isActive = true
+        searchTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 600).isActive = true
+        searchTextField.returnKeyType = .go
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: searchTextField.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+                
+        tableView.dataSource = self
+        
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor, constant: 400).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            //centerViewOnUserLocation()
+        }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+
+    private func checkLocationAuthorization() {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            mapView.showsUserLocation = true
+            locationManager.startUpdatingLocation()
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+
+    let scale: CGFloat = 300
+
+    private func centerViewOnUserLocation() {
+        if let coordinate = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: coordinate,
+                                                 latitudinalMeters: scale,
+                                                 longitudinalMeters: scale)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchCompleter.queryFragment = searchText
+    }
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+        tableView.reloadData()
+    }
+    
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        print("Error: \(error.localizedDescription)")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        let searchResult = searchResults[indexPath.row]
+        cell.textLabel?.text = searchResult.title
+        cell.detailTextLabel?.text = searchResult.subtitle
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedResult = searchResults[indexPath.row]
+        print("Selected: \(selectedResult.title), \(selectedResult.subtitle)")
+        
+        // Handle the selected result (e.g., perform a search, update UI, etc.)
+        convertAddressToAnnotation(name: selectedResult.title, address: selectedResult.subtitle, camera: true)
+    }
+    
+    func convertAddressToAnnotation(name: String, address: String, camera: Bool = false) {
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            guard let placemarks = placemarks, let location = placemarks.first?.location else {
+                print("No location found")
+                return
+            }
+            // Use the location (latitude, longitude)
+            let coordinate = location.coordinate
+            print("Latitude: \(coordinate.latitude), Longitude: \(coordinate.longitude)")
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            //you can add whatever info you want to your pin
+            annotation.title = name
+            for a in self.annotationList {
+                self.mapView.removeAnnotation(a)
+            }
+            self.mapView.addAnnotation(annotation)
+            self.annotationList.append(annotation)
+            if (camera) {
+                if let userLocation = self.locationManager.location?.coordinate {
+                    self.centerMapOnCoordinates(coord1: userLocation, coord2: coordinate)
+                }
+            }
+        }
+    }
+    
+    func centerMapOnCoordinates(coord1: CLLocationCoordinate2D, coord2: CLLocationCoordinate2D) {
+        let camera = MKMapCamera()
+        
+        let midLatitude = (coord1.latitude + coord2.latitude) / 2.0
+        let midLongitude = (coord1.longitude + coord2.longitude) / 2.0
+        let center = CLLocationCoordinate2D(latitude: midLatitude, longitude: midLongitude)
+        
+        let location1 = CLLocation(latitude: coord1.latitude, longitude: coord1.longitude)
+        let location2 = CLLocation(latitude: coord2.latitude, longitude: coord2.longitude)
+        let distance = location1.distance(from: location2)
+        let bearing = calculateBearing(from: coord1, to: coord2)
+        
+        // This is currently not super robust, needs to be dialed in
+        // Also blocked by the temporary UI
+        camera.centerCoordinate = center
+        camera.centerCoordinateDistance = 4.5 * distance
+        camera.heading = bearing
+        
+        mapView.setCamera(camera, animated: true)
+    }
+    
+    func calculateBearing(from coordinate1: CLLocationCoordinate2D, to coordinate2: CLLocationCoordinate2D) -> CLLocationDirection {
+        let deltaLongitude = coordinate2.longitude - coordinate1.longitude
+        let y = sin(deltaLongitude) * cos(coordinate2.latitude)
+        let x = cos(coordinate1.latitude) * sin(coordinate2.latitude) - sin(coordinate1.latitude) * cos(coordinate2.latitude) * cos(deltaLongitude)
+        let initialBearing = atan2(y, x) * 180 / .pi
+        let compassBearing = (initialBearing + 360).truncatingRemainder(dividingBy: 360) // Normalize to 0-360
+        print(compassBearing)
+        return compassBearing
+    }
+}
