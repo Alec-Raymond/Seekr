d3a7619aa2598cde8d895aaf7e0d67598e17f7fa
Aidan Dhillon <aidandhillon@macbookpro.lan>
2024-11-06 17:16:24 -0800

    Profile View UI fix

--- Seekr/Core/Profile/ProfileView.swift
+++ Seekr/Core/Profile/ProfileView.swift
@@ -19,53 +19,106 @@ struct ProfileView: View {
     @State private var userData: AppUser?
     @State private var isLoading = false
     @State private var errorMessage = ""
-
+    
+    private var initials: String {
+        guard let fullname = userData?.fullname else {return ""}
+        return parseInitials(from: fullname)
+    }
+    
     var body: some View {
         VStack(spacing: 20) {
             if isLoading {
                 ProgressView("Loading...")
                     .progressViewStyle(CircularProgressViewStyle())
-            } else if let user = userData {
-                Text("Welcome, \(user.fullname)")
-                    .font(.title)
-                    .bold()
-
-                Text("Email: \(user.email)")
-                    .foregroundColor(.gray)
-
-                Button(action: {
-                    authViewModel.signOut()
-                }) {
-                    Text("Log Out")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.red)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
-                        .padding(.horizontal)
+            }
+            else if let user = userData {
+                List {
+                    Section {
+                        HStack {
+                            if #available(iOS 17.0, *) {
+                                Text(initials)
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(Color(.white))
+                                    .frame(width: 72, height: 72)
+                                    .background(Color(.blue.opacity(0.7)))
+                                    .clipShape(Circle())
+                            } else {
+                                // Fallback on earlier versions
+                            }
+                            
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text(user.fullname)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .padding(.top, 4)
+                                
+                                Text(user.email)
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                    }
+                    
+                    Section("General") {
+                        HStack {
+                            SettingsRowView(imageName: "gear",
+                                            title: "Version",
+                                            tintColor: .accentColor)
+                            Spacer()
+                            Text("1.0.0")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    
+                    Section("Account") {
+                        Button {
+                            Task{
+                                
+                                authViewModel.signOut()
+                            }
+                        } label: {
+                            SettingsRowView(imageName: "arrow.left.circle.fill",
+                                            title: "Sign out",
+                                            tintColor: .accentColor)
+                        }
+                        
+                        Button {
+                            Task{
+                                try await
+                                authViewModel.deleteAccount()
+                            }
+                        } label: {
+                            SettingsRowView(imageName: "xmark.circle.fill",
+                                            title: "Delete account",
+                                            tintColor: .red)
+                        }
+                    }
                 }
-
-                Spacer()
-            } else {
+            }
+            else {
                 Text("No user data available.")
                     .foregroundColor(.gray)
                     .padding()
             }
-
+            
             if !errorMessage.isEmpty {
                 Text(errorMessage)
                     .foregroundColor(.red)
                     .padding(.horizontal)
             }
+
         }
-        .padding()
-        .navigationTitle("Profile")
-        .onAppear {
-            loadUserData()
-        }
+        
+            .padding()
+            .navigationTitle("Profile")
+            .task {
+                 await loadUserData()
+             }
     }
 
-    private func loadUserData() {
+    private func loadUserData() async {
         Task {
             isLoading = true
             await authViewModel.fetchUser()
@@ -73,7 +126,16 @@ struct ProfileView: View {
             isLoading = false
         }
     }
-}
+    private func parseInitials(from fullname: String) -> String{
+            let formatter = PersonNameComponentsFormatter()
+            if let components = formatter.personNameComponents (from: fullname) {
+                formatter.style = .abbreviated
+                return formatter.string (from: components)
+            }
+            return ""
+        }
+    }
+
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
